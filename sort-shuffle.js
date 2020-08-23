fcdc29e747c15acb99c8dfa24cb7ead788d8464e
shriramcs <shriramcs@gmail.com>
2020-08-23 10:20:57 +0530

    factory pattern added

--- sort-shuffle.js
+++ sort-shuffle.js
@@ -1,86 +1,100 @@
 (function iife(){
     const globalBoxes = [1,2,3,4,5,6,7,8,9];
-    const MAX_BOXES = 9;
     
-    /**
-     * we can optimize the sort method
-     * 
-     * @param {boxes} boxes 
-     */
-    function optimizedSort(boxes) {
-        return boxes.sort((a, b) => a-b);
-    }
-    
-    function getRandomIndexes() {
-        const arr = [];
-        let count = 0;
-
-        while(arr.length < MAX_BOXES){
-            const randomVal = Math.floor(Math.random() * MAX_BOXES);
-            if((randomVal >= 0 || randomVal <= (MAX_BOXES - 1)) && !arr.includes(randomVal)) {
-                arr.push(randomVal);
-            }
-            count++;
-        }
-        console.log('shuffled array', arr);
-        return arr;
-    }
-
-    function getBox(box) {
-        const boxEl = document.createElement('div');
-        const textEl = document.createTextNode(box.toString());
-        boxEl.appendChild(textEl);
-        boxEl.className = `box color-${box}`
-        return boxEl;
-    }
-
-    /**
-     * Method to construct HTML code for boxes and attach it to container element
-     * @param {array of boxes to display} boxes 
-     */
-    function displayBoxes(boxes){
-        const containerRef = document.getElementById('boxContainer');
-        if(!boxContainer){
-            console.log('Element with boxContainer id does not exist!!!');
+    const sortShuffle = function fe(params) {
+        if(!(params && params.length > 0)){
+            throw Error('Box list passed is not valid, ex: [1,2,3,4]');
             return;
         }
-        containerRef.innerHTML = '';
-        boxes.forEach(box => {
-            const boxEl = getBox(box);
+        const MAX_BOXES = params.length;
+
+        /**
+         * we can optimize the sort method
+         * 
+         * @param {boxes} boxes 
+         */
+        function optimizedSort(boxes) {
+            return boxes.sort((a, b) => a-b);
+        }
+        
+        function getRandomIndexes() {
+            const arr = [];
+            let count = 0;
+
+            while(arr.length < MAX_BOXES){
+                const randomVal = Math.floor(Math.random() * MAX_BOXES);
+                if((randomVal >= 0 || randomVal <= (MAX_BOXES - 1)) && !arr.includes(randomVal)) {
+                    arr.push(randomVal);
+                }
+                count++;
+            }
             
-            containerRef.appendChild(boxEl);
-        })
-        
+            return arr;
+        }
 
-        console.log('boxes displayed');
-    }
+        function getBox(box) {
+            const boxEl = document.createElement('div');
+            const textEl = document.createTextNode(box.toString());
+            boxEl.appendChild(textEl);
+            boxEl.className = `box color-${box}`
+            return boxEl;
+        }
 
-    /**
-     * Method to shuffle the displayed boxes.
-     * 
-     * @param {Array of boxes} boxes 
-     */
-    function shuffle(boxes) {
-        const newBoxes = [];
-        
-        getRandomIndexes().forEach((indexVal, index) => newBoxes[index] = boxes[indexVal]);
-        displayBoxes(newBoxes);
-    }
+        /**
+         * Method to construct HTML code for boxes and attach it to container element
+         * @param {array of boxes to display} boxes 
+         */
+        function displayBoxes(boxes){
+            const containerRef = document.getElementById('boxContainer');
+            if(!boxContainer){
+                console.log('Element with boxContainer id does not exist!!!');
+                return;
+            }
+            containerRef.innerHTML = '';
+            boxes.forEach(box => {
+                const boxEl = getBox(box);
+                
+                containerRef.appendChild(boxEl);
+            })
+        }
 
-    /**
-     * Method to sort the displayed boxes
-     * 
-     * @param {array of boxes} boxes 
-     */
-    function sort(boxes){
-        const sortedArray = optimizedSort(boxes);
+        /**
+         * Method to shuffle the displayed boxes.
+         * 
+         * @param {Array of boxes} boxes 
+         */
+        function shuffle(boxes) {
+            const newBoxes = [];
+            
+            getRandomIndexes().forEach((indexVal, index) => newBoxes[index] = boxes[indexVal]);
+            displayBoxes(newBoxes);
+        }
 
-        displayBoxes(sortedArray);
-    }
+        /**
+         * Method to sort the displayed boxes
+         * 
+         * @param {array of boxes} boxes 
+         */
+        function sort(boxes){
+            const sortedArray = optimizedSort(boxes);
+
+            displayBoxes(sortedArray);
+        }
+
+        return {
+            sort,
+            shuffle,
+            displayBoxes
+        }
+
+    };
+
+    
+    const gamified = sortShuffle(globalBoxes);
     
 
     //initial display of numbered boxes
-    displayBoxes(globalBoxes);
+    gamified.displayBoxes(globalBoxes);
 
     
     // Event handlers for sort and shuffle buttons
@@ -89,11 +103,11 @@
     const shuffleButton = document.getElementById('shuffleBtn');
 
     sortButton.addEventListener('click', () => {
-        sort(globalBoxes);
+        gamified.sort(globalBoxes);
     });
 
     shuffleButton.addEventListener('click', () => {
-        shuffle(globalBoxes);
+        gamified.shuffle(globalBoxes);
     });
 
 }());
